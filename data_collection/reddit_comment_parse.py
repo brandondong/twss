8d920e5d48c8e43ce3078fef4b8b5c9ba2fa5923
Brandon <brandondong604@hotmail.com>
2018-12-18 15:34:14 -0800

    Implement formatting stripping

--- data_collection/reddit_comment_parse.py
+++ data_collection/reddit_comment_parse.py
@@ -4,12 +4,25 @@ LENNY_FACE = "( ͡° ͜ʖ ͡°)"
 TWSS_LETTERS_TEST = "thatswhatshesaid"
 
 QUOTE_FORMAT = re.compile(r">(.+)\n+(.+)")
-NON_ALPHANUMERIC = re.compile(r"[^\w]")
+NON_ALPHANUMERIC = re.compile(r"[^\w]|_")
 
 # Two different ways of representing bold text.
 BOLD_1 = re.compile(r"\*\*(.+)\*\*")
 BOLD_2 = re.compile(r"__(.+)__")
-	
+
+NONESCAPED_UNDERSCORE = re.compile(r"(^|[^\\])_")
+NONESCAPED_ASTERIX = re.compile(r"(^|[^\\])\*")
+
+NONESCAPED_DOUBLE_UNDERSCORE = re.compile(r"(^|[^\\])__")
+NONESCAPED_DOUBLE_ASTERIX = re.compile(r"(^|[^\\])\*\*")
+
+SUPERSCRIPT = re.compile(r"([^\\])\^")
+
+STRIKETHROUGH_STARTING_SPACE = re.compile(r" ~~.+~~")
+STRIKETHROUGH_ELSE = re.compile(r"(^|[^\\])~~.+~~ ?")
+
+ESCAPE_SEQUENCE = re.compile(r"(^|[^\\])\\")
+
 def match_twss(s):
 	"""Matches for the delivery of a 'That's what she said' punchline against the comment text.
 
@@ -29,10 +42,32 @@ def match_twss(s):
 	punchline = matched.group(2)
 	if not _is_punchline(punchline):
 		return None
-	joke = matched.group(1).strip()
+	joke = matched.group(1)
 	# Since quoting requires copying and pasting plaintext, any bolding can be assumed to be emphasizing the actual joke.
-	return _extract_possible_bold(joke)
+	return strip_formatting(_extract_possible_bold(joke))
 	
+def strip_formatting(s):
+	"""Strips the comment text of any Reddit formatting. Assumed not to have quotes."""
+	
+	# Handle bolded text.
+	s = NONESCAPED_DOUBLE_UNDERSCORE.sub(r"\1", s)
+	s = NONESCAPED_DOUBLE_ASTERIX.sub(r"\1", s)
+	
+	# Handle italicized text.
+	s = NONESCAPED_UNDERSCORE.sub(r"\1", s)
+	s = NONESCAPED_ASTERIX.sub(r"\1", s)
+	
+	# Handle superscripts.
+	s = SUPERSCRIPT.sub(r"\1 ", s)
+	
+	# Handle strikethroughs.
+	s = STRIKETHROUGH_STARTING_SPACE.sub("", s)
+	s = STRIKETHROUGH_ELSE.sub(r"\1", s)
+	
+	# Remove the escapes now that all formatting is removed.
+	s = ESCAPE_SEQUENCE.sub(r"\1", s)
+	return s.strip()
+
 def _is_punchline(s):
 	return _is_lenny(s) or _is_twss(s)
 	
--- tests/data_collection/test_reddit_comment_parse.py
+++ tests/data_collection/test_reddit_comment_parse.py
@@ -15,6 +15,7 @@ class TestRedditCommentParse(unittest.TestCase):
 		self.assertEqual(rcp.match_twss("That's what she said!"), "")
 		self.assertEqual(rcp.match_twss("THAT'S WHAT SHE SAID"), "")
 		self.assertEqual(rcp.match_twss("thats what she said."), "")
+		self.assertEqual(rcp.match_twss("thats_what_she_said"), "")
 		self.assertEqual(rcp.match_twss("  ( ͡° ͜ʖ ͡°)"), "")
 		self.assertEqual(rcp.match_twss("( ͡° ͜ʖ ͡°) "), "")
 		self.assertEqual(rcp.match_twss("( ͡° ͜ʖ ͡°)\n"), "")
@@ -36,3 +37,34 @@ class TestRedditCommentParse(unittest.TestCase):
 		# A real comment: https://www.reddit.com/r/programming/comments/9sc0qj/deepcreampy_decensoring_hentai_with_deep_neural/e8npbse.
 		test_comment = ">Image Inpainting for **Irregular Holes** Using Partial Convolutions\n\n( ͡° ͜ʖ ͡°)"
 		self.assertEqual(rcp.match_twss(test_comment), "Irregular Holes")
+		
+	def test_match_strips_formatting(self):
+		self.assertEqual(rcp.match_twss(">_It's_ hard\n\nThat's what she said"), "It's hard")
+		self.assertEqual(rcp.match_twss(">It's *hard*\n\nThat's what she said"), "It's hard")
+		
+	def test_strip_italics(self):
+		self.assertEqual(rcp.strip_formatting("_Hello_"), "Hello")
+		self.assertEqual(rcp.strip_formatting("*Hello*"), "Hello")
+		self.assertEqual(rcp.strip_formatting("Hello *World!*"), "Hello World!")
+		self.assertEqual(rcp.strip_formatting("Hello _World!_"), "Hello World!")
+		
+	def test_strip_bold(self):
+		self.assertEqual(rcp.strip_formatting("__Hello__"), "Hello")
+		self.assertEqual(rcp.strip_formatting("**Hello**"), "Hello")
+		self.assertEqual(rcp.strip_formatting("Hello **World!**"), "Hello World!")
+		self.assertEqual(rcp.strip_formatting("Hello __World!__"), "Hello World!")
+		
+	def test_strip_superscript(self):
+		self.assertEqual(rcp.strip_formatting("Hello^World!"), "Hello World!")
+		
+	def test_strip_strikethrough(self):
+		self.assertEqual(rcp.strip_formatting("This is ~~not~~ a joke"), "This is a joke")
+		self.assertEqual(rcp.strip_formatting("I should finish ~~quickly~~."), "I should finish.")
+		self.assertEqual(rcp.strip_formatting("~~what's~~ updog"), "updog")
+		self.assertEqual(rcp.strip_formatting("fire~~truck~~"), "fire")
+		self.assertEqual(rcp.strip_formatting("~~fire~~truck"), "truck")
+		
+	def test_strip_escapes(self):
+		self.assertEqual(rcp.strip_formatting("3 \* 4"), "3 * 4")
+		self.assertEqual(rcp.strip_formatting(r"3\\4"), r"3\4")
+		self.assertEqual(rcp.strip_formatting("3\^4"), "3^4")
